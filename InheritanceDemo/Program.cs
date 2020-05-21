0a109d80b5090d77bd82fdd3a17f9bb5f20d34b4
Derek Sparks <drsparks@gmail.com>
2020-05-21 16:49:54 -0700

    Add project files.

---
+++ InheritanceDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Car
+    {
+        public int NumberOfWheels { get; set; }
+        public int NumberOfDoors { get; set; }
+
+        public void StopCar()
+        {
+
+        }
+
+        public void StartCar()
+        {
+
+        }
+    }
+
+    public class Corrola : Car
+    {
+
+    }
+}
